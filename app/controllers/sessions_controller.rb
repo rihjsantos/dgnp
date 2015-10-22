c91526dfcbead65032a99d7ea8fc0a235c0fc6d2
Ricardo <ricardo@ebanx.com>
2015-10-22 18:29:01 -0200

    Initial admin area ok. Foward to CRUD items

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,27 @@ class SessionsController < ApplicationController
  	include SessionsHelper
  	layout "login"
 
+ 	# Login page
  	def new
  	end
 
+ 	# Authenticate the user
  	def create
- 		
+ 		user = User.find_by(email: params[:session][:email].downcase)
+	    if user && user.authenticate(params[:session][:password])
+			# SessionsHelper method
+			log_in user
+      		redirect_to :controller => 'admin', :action => 'index'
+	    else
+      		flash[:danger] = 'Combinação de e-mail/senha incorreta'
+      		render 'new'
+    	end
  	end
+
+ 	# Logout the user
+ 	def destroy
+		session.delete(:user_id)
+		@current_user = nil
+		redirect_to :controller => 'sessions', :action => 'new'
+	end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,13 +10,13 @@ module SessionsHelper
 	end
 
 	# Returns true if the user is logged in, false otherwise.
-	def logged_in		
+	def logged_in?
 		!current_user.nil?
   	end
 
   	# Confirms a logged-in user or redirect
 	def logged_in_user				
-		unless !:logged_in?
+		unless :logged_in?
 			flash[:danger] = "Please log in."
 			redirect_to login_url
 		end

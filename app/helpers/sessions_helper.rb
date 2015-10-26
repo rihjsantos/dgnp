8fb13dd28c4b2fde27abcd3ef03d3d6cc6fad506
Ricardo <ricardo@ebanx.com>
2015-10-26 18:14:29 -0200

    Admin adjustment

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,7 +16,7 @@ module SessionsHelper
 
   	# Confirms a logged-in user or redirect
 	def logged_in_user				
-		unless :logged_in?
+		if !logged_in?
 			flash[:danger] = "Please log in."
 			redirect_to login_url
 		end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020180634) do
+ActiveRecord::Schema.define(version: 20151026174908) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "description", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            limit: 255
